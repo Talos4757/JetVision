bd48a830ca4f31e747ef62660d5660400b514394
Matan Rosenberg <matan129@gmail.com>
2014-12-09 18:41:13 +0200

    Removed GUI files and made a seperate method for PreProcessing

--- axistest.cpp
+++ axistest.cpp
@@ -20,8 +20,60 @@ pthread_mutex_t frameLocker;
 
 const string videoStreamAddress = "http://10.0.0.69/mjpg/video.mjpg";
 
+void* PreProcessFrame()
+{
+	//Matrices
+	Mat currentFrame, threshed, redblue;
+	gpu::GpuMat dst, src, chan[3], ths[3], redAndBlue, erd;
+
+
+	//MUTEX critical area
+	pthread_mutex_lock(&frameLocker);
+	currentFrame = frame;
+	pthread_mutex_unlock(&frameLocker);
+	//End of Mutex critial area
+
+	if(currentFrame.empty()){
+		cout << "Recieved empty frame" << endl;
+		continue;
+	}
+
+	//GPU Code
+	src.upload(currentFrame);
+
+	gpu::split(src,chan); //Split to three channels
+
+	gpu::threshold(chan[1],ths[1],MIN_THRESH,MAX_THRESH,THRESH_BINARY); //threshold green
+
+	//Threshold the two other channels
+	gpu::threshold(chan[0],ths[0],MIN_THRESH,MAX_THRESH,THRESH_BINARY);
+	gpu::threshold(chan[2],ths[2],MIN_THRESH,MAX_THRESH,THRESH_BINARY);
+
+	//Invert the Red and Blue Channels
+	gpu::bitwise_not(ths[0],chan[0]);
+	gpu::bitwise_not(ths[2],chan[2]);
+
+	//AND it all togther
+	gpu::bitwise_and(chan[0],chan[2],redAndBlue);
+	gpu::bitwise_and(redAndBlue,ths[1],dst);
+
+	//Filter out little pseudo targets
+	gpu::erode(dst,erd,Mat(),2);
+	gpu::dilate(erd,dst,Mat());
+
+	//Download to RAM
+	dst.download(threshed);
+//	redAndBlue.download(redblue);
+
+	//imshow - disabled for performance
+//	imshow("Original Image", currentFrame);
+//	imshow("NOT Red AND NOT Blue", redblue);
+//	imshow("Threshed", threshed);
+}
+
+
 //Frame updater method which runs on seperate thread
-void *UpdateFrame(void *arg)
+void* UpdateFrame(void *arg)
 {
 	for(;;)
 	{
@@ -50,65 +102,21 @@ int main(int, char**) {
 	struct timespec start, finish;
 	double elapsed;
 
-	//Matrices
-	Mat currentFrame, threshed, redblue;
-	gpu::GpuMat dst, src, chan[3], ths[3], redAndBlue, erd;
-
-	for(;;)
+	
+	while(true)
 	{
 		//Start the clock
 		clock_gettime(CLOCK_MONOTONIC, &start);
+		//
 
-		//MUTEX critical area
-		pthread_mutex_lock(&frameLocker);
-		currentFrame = frame;
-		pthread_mutex_unlock(&frameLocker);
-		//End of Mutex critial area
-
-		if(currentFrame.empty()){
-			cout << "Recieved empty frame" << endl;
-			continue;
-		}
-
-		//GPU Code
-		src.upload(currentFrame);
-
-		gpu::split(src,chan); //Split to three channels
-
-		gpu::threshold(chan[1],ths[1],MIN_THRESH,MAX_THRESH,THRESH_BINARY); //threshold green
-
-		//Threshold the two other channels
-		gpu::threshold(chan[0],ths[0],MIN_THRESH,MAX_THRESH,THRESH_BINARY);
-		gpu::threshold(chan[2],ths[2],MIN_THRESH,MAX_THRESH,THRESH_BINARY);
-
-		//Invert the Red and Blue Channels
-		gpu::bitwise_not(ths[0],chan[0]);
-		gpu::bitwise_not(ths[2],chan[2]);
-
-		//AND it all togther
-		gpu::bitwise_and(chan[0],chan[2],redAndBlue);
-		gpu::bitwise_and(redAndBlue,ths[1],dst);
-
-		//Filter out little pseudo targets
-		gpu::erode(dst,erd,Mat());
-		gpu::dilate(erd,dst,Mat());
-
-		//Download to RAM
-		dst.download(threshed);
-//		redAndBlue.download(redblue);
-
-		//imshow - disabled for performance
-//		imshow("Original Image", currentFrame);
-//		imshow("NOT Red AND NOT Blue", redblue);
-//		imshow("Threshed", threshed);
-
-		//Clock
+		PreProcessFrame();
+		waitKey(1);
+	
+		//Display clock output
 		clock_gettime(CLOCK_MONOTONIC, &finish);
 		elapsed = (finish.tv_sec - start.tv_sec);
 		elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000.0;
 		cout << elapsed << "ms" << endl;
-
-		waitKey(1);
 	}
 }
 
