51db0553caa0c6fbf11a4a007b966715912d296e
Matan R <matan129@gmail.com>
2014-12-13 17:19:02 +0200

    Added Target struct

--- Vision/UtilityStructs.h
+++ Vision/UtilityStructs.h
@@ -5,3 +5,23 @@ struct UpdaterStruct
   //GpuMat *gpuFrame;
   pthread_mutex_t *frameLocker;
 };
+
+enum TargetType
+{
+  //MUST be ordered by in game points value (best target is last)
+  LowGoalR,
+  LowGoalL,
+  HighGoalR,
+  HighGoalL,
+  Truss,
+  HotGoal
+};
+
+struct Target
+{
+  //ALL numbers should be relative to the driving pivot!
+  TargetType type;
+  float distance;
+  float height;
+  float angle;
+};
